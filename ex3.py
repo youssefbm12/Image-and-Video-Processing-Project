5975de3b67004b828e286ce11c0956a1e1b56d04
youssefbm12 <72268364+youssefbm12@users.noreply.github.com>
2022-05-25 12:23:29 +0200

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -12,93 +12,95 @@ def rescaleImage(Image, scaler_factor):
     dimension = (width,height)
 
     return cv.resize(Image, dimension, interpolation=cv.INTER_AREA)
-img = rescaleImage(img,0.5)
+# img = rescaleImage(img,0.5)
 
-copy = img
-
-# # Convert to grayscale.
-# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-  
-# # Blur using 3 * 3 kernel.
-# gray_blurred = cv.blur(gray, (3, 3))
-# grayImage = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-  
+# copy = img
+# grayImage = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 # (thresh, blackAndWhiteImage) = cv.threshold(grayImage, 127, 255, cv.THRESH_BINARY)
- 
-# # cv.imshow('Black white image', blackAndWhiteImage)
-# # Apply Hough transform on the blurred image.
-# detected_circles = cv.HoughCircles(grayImage, cv.HOUGH_GRADIENT, 1, 20, param1 = 100, param2 = 30, minRadius = 4, maxRadius = 50)
+# cv.imshow('Black white image', blackAndWhiteImage)
 
-# count = 0
-# # Draw circles that are detected.
-# if detected_circles is not None:
-  
-#     # Convert the circle parameters a, b and r to integers.
-#     detected_circles = np.uint16(np.around(detected_circles))
-  
-#     for pt in detected_circles[0, :]:
-#         count = count +1
-#         a, b, r = pt[0], pt[1], pt[2]
-  
-#         # Draw the circumference of the circle.
-#         cv.circle(img, (a, b), r, (255, 255, 255), -1)
-  
-#         # Draw a small circle (of radius 1) to show the center.
-        
-# print(count)
-# cv.imshow("Detected Circle", img)
-# # cv.imshow("Black & White", blackAndWhiteImage)
+# gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
-gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-
-# apply gaussian blur
-blur = cv.GaussianBlur(gray, (9,9), 0)
+# # apply gaussian blur
+# blur = cv.GaussianBlur(gray, (9,9), 0)
 
 
-# threshold
-thresh = cv.threshold(blur,128,255,cv.THRESH_BINARY)[1]
+# # threshold
+# thresh = cv.threshold(blur,128,255,cv.THRESH_BINARY)[1]
 
 
-# apply close and open morphology to smooth
-kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (9,9))
-thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
-thresh = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel)
+# # apply close and open morphology to smooth
+# kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (9,9))
+# thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
+# thresh = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel)
 
-# draw contours and get centroids
-circles = img.copy()
-contours = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-contours = contours[0] if len(contours) == 2 else contours[1]
-for cntr in contours:
-    cv.drawContours(circles, [cntr], -1, (255,255,255), -1)
-    M = cv.moments(cntr)
-    cx = int(M["m10"] / M["m00"])
-    cy = int(M["m01"] / M["m00"])
-    x = round(cx)
-    y = round(cy)
+# # draw contours and get centroids
+# circles = img.copy()
+# contours = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+# contours = contours[0] if len(contours) == 2 else contours[1]
+# for cntr in contours:
+#     cv.drawContours(circles, [cntr], -1, (255,255,255), -1)
+#     M = cv.moments(cntr)
+#     cx = int(M["m10"] / M["m00"])
+#     cy = int(M["m01"] / M["m00"])
+#     x = round(cx)
+#     y = round(cy)
     
-    circles[y-2:y+3,x-2:x+3] = (0,255,0)
-    cv.circle(circles, (x, y), 5, (255, 255, 255), -1)
+#     circles[y-2:y+3,x-2:x+3] = (0,255,0)
+#     cv.circle(circles, (x, y), 5, (255, 255, 255), -1)
             
 
-cv.imshow("circles", circles)
+# cv.imshow("circles", circles)
 
-# Printing the number of oranges in the picture
-print("The number of Oranges in the Image is:",len(contours))
+# # Printing the number of oranges in the picture
+# print("The number of Oranges in the Image is:",len(contours))
 
-# Question 2 
-path = r'C:\Users\youss\OneDrive\Documents\IVP\project 2\jar.jpg'
+# Question 1
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from skimage.io import imread, imshow
+from skimage.color import rgb2gray
+from skimage.morphology import (erosion, dilation, closing, opening,
+                                area_closing, area_opening)
+from skimage.measure import label, regionprops, regionprops_table
+path = r'C:\Users\youss\OneDrive\Documents\IVP\project 2\orangetree.jpg'
 img2 = cv.imread(path)
 img2 = rescaleImage(img2,0.1)
 copy = img2
-gray = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
 
-# apply gaussian blur
-blur = cv.GaussianBlur(gray, (9,9), 0)
+def open(image, kernel):
+    eroded = cv.erode(image, kernel)
+    dilated = cv.dilate(eroded, kernel)
+    return dilated
 
 
-# threshold
-thresh = cv.threshold(blur,1 100,255,cv.THRESH_BINARY)[1]
-cv.imshow("Thresh",thresh)
-cv.imshow("Original Image",img2)
+def close(image, kernel):
+    dilated = cv.dilate(image, kernel)
+    eroded = cv.erode(dilated, kernel)
+    return eroded
+def findcirlce(image,alpha,beta):
+    grayImage = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+    (_, image) = cv.threshold(grayImage, 127, 255, cv.THRESH_BINARY)
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(7,7))
+    opening = open(image,kernel)
+    closing = close(opening, kernel)
+    label_im = label(closing)
+    regions = regionprops(label_im)
+    masks = []
+    bbox = []
+    list_of_index = []
+    for num, x in enumerate(regions):
+        area = x.area
+        convex_area = x.convex_area
+        if (num!=0 and (area>10) and (convex_area/area <alpha) #100
+        and (convex_area/area >beta)):
+            masks.append(regions[num].convex_image)
+            bbox.append(regions[num].bbox)
+            list_of_index.append(num)
+    count = len(masks)
+    print(count)
+findcirlce(img,1.05,0.95)
+findcirlce(img2,1.22,0.95)
 cv.waitKey(0)
 cv.destroyAllWindows()
